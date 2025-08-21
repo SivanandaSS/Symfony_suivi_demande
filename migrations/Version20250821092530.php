5a09a3e5dfae18aaf3958cc7a881fa22b1005d41
Roms10000 <romain.richardon@outlook.fr>
2025-08-21 11:35:23 +0200

    update plugins

--- migrations/Version20250821092530.php
+++ migrations/Version20250821092530.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250820085734 extends AbstractMigration
+final class Version20250821092530 extends AbstractMigration
 {
     public function getDescription(): string
     {
